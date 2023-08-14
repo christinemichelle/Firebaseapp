577533b101ad020ce239e819381e6390b9638114
Christinemichelle <christinekito39@gmail.com>
2023-08-14 15:05:12 +0300

    Initial commit

---
+++ app/src/main/java/com/example/firebaseapp/EmployeeDetailsActivity.kt
@@ -0,0 +1,11 @@
+package com.example.firebaseapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EmployeeDetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_employee_details)
+    }
+}
